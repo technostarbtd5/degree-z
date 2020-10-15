015fd37151c111f186b4b64496ddd0c567a6eb94
technostarbtd5 <aldenrford@yahoo.com>
2020-10-15 12:27:25 -0400

    created intial HTML for fsm; added my schedule plan as sample JSON

---
+++ client/future-semester-planner/future-semester-planner.js
@@ -0,0 +1,56 @@
+const SCHEDULE_EXAMPLE_JSON = {
+  "semesters": {
+    "transfer": [
+      {"subject": "CHEM", "course": "1100"},
+      {"subject": "CHEM", "course": "1200"},
+      {"subject": "CSCI", "course": "1100"},
+      {"subject": "MATH", "course": "1010"},
+      {"subject": "MATH", "course": "1020"},
+      {"subject": "MGMT", "course": "2100"},
+      {"subject": "PHYS", "course": "1100"},
+      {"subject": "STSH", "course": "1000"},
+      {"subject": "STSS", "course": "1000"},
+      {"subject": "STSS", "course": "1000"},
+      {"subject": "WRIT", "course": "1000"},
+    ],
+    "Fall 2019": [
+      {"subject": "BIOL", "course": "1010"},
+      {"subject": "BIOL", "course": "1015"},
+      {"subject": "CSCI", "course": "1200"},
+      {"subject": "IHSS", "course": "1200"},
+      {"subject": "MATH", "course": "2010"},
+    ],
+    "Spring 2020": [
+      {"subject": "CSCI", "course": "2200"},
+      {"subject": "CSCI", "course": "2500"},
+      {"subject": "ECON", "course": "2020"},
+      {"subject": "ITWS", "course": "1100"},
+      {"subject": "ITWS", "course": "1220"},
+    ],
+    "Fall 2020": [
+      {"subject": "CSCI", "course": "2300"},
+      {"subject": "MATH", "course": "4100"},
+      {"subject": "COMM", "course": "4420"},
+      {"subject": "ECON", "course": "4130"},
+      {"subject": "ITWS", "course": "2110"},
+    ],
+    "Spring 2021": [
+      {"subject": "CSCI", "course": "2600"},
+      {"subject": "CSCI", "course": "4150"},
+      {"subject": "ITWS", "course": "2210"},
+      {"subject": "ITWS", "course": "4310"},
+      {"subject": "ITWS", "course": "4500"},
+    ],
+    "Fall 2021": [
+      {"subject": "CSCI", "course": "4100"},
+      {"subject": "CSCI", "course": "4220"},
+      {"subject": "CSCI", "course": "4430"},
+      {"subject": "ITWS", "course": "4100"},
+      {"subject": "ECOM", "course": "2010"},
+    ],
+    "Spring 2022": [
+      {"subject": "CSCI", "course": "4210"},
+      {"subject": "CSCI", "course": "4380"},
+    ],
+  }
+}
