a814a2fbcdb600f5f6231e9d39fab6660c2307cf
technostarbtd5 <aldenrford@yahoo.com>
2020-12-09 16:08:13 -0500

    phpCAS should no longer break things with its echo statement

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once(__DIR__ ."/phpCAS-1.3.6/CAS.php");
 phpCAS::client(CAS_VERSION_2_0, 'cas-auth.rpi.edu', 443, '/cas/');
 phpCAS::setCasServerCACert(__DIR__ .'/cacert.pem');
 if (phpCAS::isAuthenticated()) {
-  echo "User:" . phpCAS::getUser();
+  // echo "User:" . phpCAS::getUser();
   // echo "<a href='./logout.php'>Logout</a>";
 } else {
   phpCAS::forceAuthentication();
